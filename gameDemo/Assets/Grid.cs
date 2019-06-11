fb2568940b1a48fe842b0da86e77adad840514f8
Betarabbit14 <1048343184@qq.com>
2019-06-11 17:59:41 +0800

    Added grid and placement

---
+++ gameDemo/Assets/Grid.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+    //[SerializeField]
+    private float size = 1f;
+
+    public Vector3 GetNearestPointOnGrid(Vector3 position)
+    {
+        position -= transform.position;
+
+        int posX = Mathf.RoundToInt(position.x / size);
+        int posY = Mathf.RoundToInt(position.y / size);
+        int posZ = Mathf.RoundToInt(position.z / size);
+
+        Vector3 result = new Vector3(
+            (float)posX * size,
+            (float)posY * size,
+            (float)posZ * size);
+
+        return result;
+    }
+
+    //grid point highlight
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        for(float x = 0; x < 40; x += size)
+        {
+            for(float z = 0; z < 40; z += size)
+            {
+                var point = GetNearestPointOnGrid(new Vector3(x, 0f, z));
+                Gizmos.DrawSphere(point, .1f);
+            }
+        }
+    }
+}
---
+++ gameDemo/Assets/PlacerScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlacerScript : MonoBehaviour
+{
+    private Grid grid;
+    public Vector3 pp = new Vector3(1, 1, 1);
+
+    private void Awake()
+    {
+        grid = FindObjectOfType<Grid>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray, out hitInfo))
+            {
+                PlaceCubeNear(hitInfo.point);
+            }
+        }
+    }
+    private void PlaceCubeNear(Vector3 clickPoint)
+    {
+        var finalPosition = grid.GetNearestPointOnGrid(clickPoint);
+        GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = finalPosition;
+
+        //GameObject.CreatePrimitive(PrimitiveType.Sphere).transform.position = nearPoint;
+    }
+}
